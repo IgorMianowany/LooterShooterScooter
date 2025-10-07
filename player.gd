994b7f24ebbf3a4cac2957cda8dc746a723273cc
Igor Mianowany <fimiany@gmail.com>
2025-10-07 09:56:47 +0200

    refactor and exit logic

--- player.gd
+++ player.gd
@@ -1,22 +1,26 @@
 class_name Player
 extends CharacterBody3D
 
+const TILT_LOWER_LIMIT := deg_to_rad(-90.0)
+const TILT_UPPER_LIMIT := deg_to_rad(90.0)
 
 var speed : float = 100
 const JUMP_VELOCITY = 4.5
-var _mouse_input : bool = false
-var _mouse_rotation : Vector3
-var _rotation_input : float
-var _tilt_input : float
-var _player_rotation : Vector3
-var _camera_rotation : Vector3
-@export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
-@export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
-@export var CAMERA_CONTROLLER : Camera3D
-@export var MOUSE_SENSITIVITY : float = 0.15
+var mouse_input : bool = false
+var mouse_rotation : Vector3
+var rotation_input : float
+var tilt_input : float
+var player_rotation : Vector3
+var camera_rotation : Vector3
+
+@export var camera_controller : Camera3D
+@export var mouse_sensitivity : float = 0.15
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _exit():
+	get_tree().quit()
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -37,29 +41,33 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
-		
+	
+	
 	_update_camera(delta)
 	move_and_slide()
 	
 func _unhandled_input(event):
-	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
-	if _mouse_input :
-		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
-		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
+	mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	if mouse_input:
+		rotation_input = -event.relative.x * mouse_sensitivity
+		tilt_input = -event.relative.y * mouse_sensitivity
+	if event.is_action_pressed("exit"):
+		_exit()
+
 
 func _update_camera(delta):
 	
-	_mouse_rotation.x += _tilt_input * delta
-	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
-	_mouse_rotation.y += _rotation_input * delta
+	mouse_rotation.x += tilt_input * delta
+	mouse_rotation.x = clamp(mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
+	mouse_rotation.y += rotation_input * delta
 	
-	_player_rotation = Vector3(0.0,_mouse_rotation.y,0.0)
-	_camera_rotation = Vector3(_mouse_rotation.x,0.0,0.0)
+	player_rotation = Vector3(0.0,mouse_rotation.y,0.0)
+	camera_rotation = Vector3(mouse_rotation.x,0.0,0.0)
 	
-	CAMERA_CONTROLLER.transform.basis = Basis.from_euler(_camera_rotation)
-	CAMERA_CONTROLLER.rotation.z = 0.0
+	camera_controller.transform.basis = Basis.from_euler(camera_rotation)
+	camera_controller.rotation.z = 0.0
 	
-	global_transform.basis = Basis.from_euler(_player_rotation)
+	global_transform.basis = Basis.from_euler(player_rotation)
 	
-	_rotation_input = 0.0
-	_tilt_input = 0.0
+	rotation_input = 0.0
+	tilt_input = 0.0
