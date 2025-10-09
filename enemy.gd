4b0213ced808a215729abd6bd2fafdfe8ec0ec8a
Igor Mianowany <fimiany@gmail.com>
2025-10-09 22:33:58 +0200

    added basic shooting animation for pistol

--- enemy.gd
+++ enemy.gd
@@ -6,7 +6,7 @@ var health : float = 100
 func _ready() -> void:
 	$SubViewport/TextureProgressBar.max_value = health
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	$SubViewport/TextureProgressBar.value = health
 
 func take_damage(damage : float):
--- pistol.gd
+++ pistol.gd
@@ -3,14 +3,17 @@ extends Weapon
 
 var shot : bool = false
 var bullet_scene := preload("res://bullet.tscn")
+var original_transform : Transform3D
 
 func _ready() -> void:
 	stop_shooting.connect(handle_stop_shooting)
 	active = true
-	bullet_spawn = $Marker3D
+	bullet_spawn = $BulletSpawn
+	original_transform = $GunModel.transform
 
 func _shoot():
 	if not shot:
+		animate_recoil()
 		shot = true
 		var bullet : Bullet = bullet_scene.instantiate()
 		add_child(bullet)
@@ -20,3 +23,8 @@ func _shoot():
 
 func handle_stop_shooting():
 	shot = false
+	
+func animate_recoil():
+	var tween = get_tree().create_tween()
+	tween.tween_property($GunModel, "transform", Transform3D($GunModel.transform).rotated(Vector3(1,0,0).normalized(), deg_to_rad(45)), .1)
+	tween.tween_property($GunModel, "transform", original_transform, .2)
