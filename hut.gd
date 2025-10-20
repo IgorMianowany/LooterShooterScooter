7499b0749fa4e42d5a2bac908b609f8ee8f77313
Igor Mianowany <fimiany@gmail.com>
2025-10-20 21:12:52 +0200

    small bugfixes

--- hut.gd
+++ hut.gd
@@ -2,3 +2,22 @@ class_name Hut
 extends StaticBody3D
 
 var storage : Array[Item]
+
+func create_loot_window() -> LootWindow:
+	var loot_window = load("res://loot_window.tscn").instantiate()
+	for item in storage:
+		loot_window.add_item(item)
+	return loot_window
+
+
+
+
+func _on_loot_range_area_entered(area: Area3D) -> void:
+	(area.get_parent() as Player).show_loot_ui(create_loot_window())
+
+
+
+func _on_loot_range_area_exited(area: Area3D) -> void:
+	(area.get_parent() as Player).hide_loot_ui()
+
+			
