06428649bb5c80b94d9a38349ad199d51a7d4dc4
Igor Mianowany <fimiany@gmail.com>
2025-10-26 20:54:40 +0100

    second obstacle type

--- game_manager.gd
+++ game_manager.gd
@@ -2,7 +2,7 @@ class_name GameManager
 extends Node
 
 @export var obstacles : Node3D
-var speed : float = 10
+@export var speed : float = 10
 
 func _process(delta: float) -> void:
 	obstacles.global_position.z += speed * delta
